ac575766b129bd53af2ff70dfaf3feed553adc56
Jarosław Kątnik <jarek@katnik.pl>
2021-11-16 20:25:24 +0100

    configuration, SRT subtitles,  new sample from Udemy

--- generate-audio.py
+++ generate-audio.py
@@ -13,67 +13,83 @@ import boto3
 from pydub import AudioSegment
 import pydub.playback
 import io
-import webvtt # for subtitles parsing
-import time
+import webvtt  # for subtitles parsing
 import moviepy.editor as mpe
 
-def synthesize(text, max_duration):
+
+class InlineClass(object):
+    def __init__(self, dict):
+        self.__dict__ = dict
+
+
+def synthesize(text, config):
     sentence = f'''
     <speak>
-         <prosody rate="medium">
-              {text}.
+         <prosody>
+              {text}
          </prosody>
     </speak>
     '''
 
     response = polly_client.synthesize_speech(
-        Engine = 'standard', # standard|neural - neural nie obsługuje max-duration
-        VoiceId='Matthew',
+        Engine='standard',  # standard|neural - neural nie obsługuje max-duration
+        VoiceId= config.voice,
         LanguageCode='en-US',
         OutputFormat='mp3',
-        TextType='ssml', # or text
+        TextType='ssml',  # or text
         Text=sentence
     )
-
 
     raw = response['AudioStream'].read()
     response['AudioStream'].close()
 
     return AudioSegment.from_mp3(io.BytesIO(raw))
 
-def caption_duration(caption):
-    end = time.mktime(time.strptime(caption.end, '%H:%M:%S.%f'))
-    start = time.mktime(time.strptime(caption.start, '%H:%M:%S.%f'))
-    return (end - start)
 
 def caption_start(caption):
     nums = [float(n) for n in caption.start.split(':')]
     seconds = nums[0] * 3600 + nums[1] * 60 + nums[2]
     return seconds
 
+
+def load_captions(config):
+    if config.captions_format == 'ttv':
+        return webvtt.read(f'input/{config.captions_file_name}')
+    elif config.captions_format == 'srt':
+        return webvtt.from_srt(f'input/{config.captions_file_name}')
+    else:
+        raise Exception('Unsupported subtitles format')
+
+
 if __name__ == '__main__':
+    config = InlineClass({
+        'captions_file_name': 'udemy_sample_02.srt',
+        'movie_file_name': 'udemy_sample_02.mp4',
+        'audio_file_name': 'udemy_sample_02.mp3',
+        'captions_format': 'srt',  # srt | ttv
+        'voice': 'Joey',  # Joey | Matthew
+    })
+
     polly_client = boto3.Session().client('polly')
 
     audio = AudioSegment.silent(duration=0)
 
-    captions = webvtt.read('input/udemy_sample_01.vtt') #[:3]
-    
+    captions = load_captions(config)
+
     for caption in captions:
-        expected_duration = caption_duration(caption)
-        sentence = synthesize(caption.text, expected_duration)
+        print(f'Processing {caption}')
+        sentence_audio = synthesize(caption.text, config)
 
         start = caption_start(caption)
         if audio.duration_seconds < start:
             break_length = (start - audio.duration_seconds) * 1000
             audio_break = AudioSegment.silent(break_length)
             audio = audio + audio_break
-        audio = audio + sentence
-    audio.export('output/udemy_sample_01.mp3', format='mp3')
-    # # pydub.playback.play(audio)
+        audio = audio + sentence_audio
+    audio.export(f'output/{config.audio_file_name}', format='mp3')
 
-    input_clip = mpe.VideoFileClip('input/udemy_sample_01.mp4')
-    new_audio = mpe.AudioFileClip('output/udemy_sample_01.mp3')
+    input_clip = mpe.VideoFileClip(f'input/{config.movie_file_name}')
+    new_audio = mpe.AudioFileClip(f'output/{config.audio_file_name}')
     # new_audio = mpe.CompositeAudioClip([input_clip.audio, new_audio])
     final_clip = input_clip.set_audio(new_audio)
-    final_clip.write_videofile("output/udemy_sample_01.mp4")
-
+    final_clip.write_videofile(f'output/{config.movie_file_name}')
